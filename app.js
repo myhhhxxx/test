0364bb68e5e795ee0abc2680c588c53a0d7898c4
myhhhxxx <60460086+myhhhxxx@users.noreply.github.com>
2020-01-29 23:12:12 -0800

    Update address

---
+++ app.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const fs = require('fs');
+
+const gitToken = fs.readFileSync('./git-token');
+
+request.get('https://api.ipify.org', (err, res, body) => {
+    const existingIp = fs.readFileSync('./ip').toString();
+
+    if (existingIp === body) {
+        fs.writeFileSync('./ip', body);
+    }
+
+    require('child_process').exec('git add .', (err, stdout) => {
+        console.log(stdout);
+
+        require('child_process').exec('git commit -m "Update address"', (error, stdout) => {
+            require('child_process').exec('git push');
+        });
+    });
+
+});
